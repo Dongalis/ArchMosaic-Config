c8fbecda0ce01f2b0b849aa144971197738dfd90
Dongalis <47064326+Dongalis@users.noreply.github.com>
2026-03-15 00:35:30 +0100

    Adding dev-ai (opencode) and removing not working themeing setup

--- profiles/hyprland/post-install.sh
+++ profiles/hyprland/post-install.sh
@@ -2,3 +2,5 @@ systemctl --user enable --now hyprpolkitagent.service
 elephant service enable
 systemctl --user enable --now waybar.service
 sed "s/#\(HandlePowerKey=\)poweroff/\1ignore/g" /etc/systemd/logind.conf | sudo tee /etc/systemd/logind.conf >/dev/null
+sudo systemctl enable --now systemd-oomd
+sudo systemctl enable --now power-profiles-daemon
